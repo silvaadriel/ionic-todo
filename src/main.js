a9c7697def67c562606fe10b22b12b7b620ae1c5
Adriel da Silva <silvaadriel510@gmail.com>
2019-09-11 15:58:46 -0300

    initial structure

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import IonicVue from '@ionic/vue';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
+
+Vue.config.productionTip = false;
+
+Vue.use(IonicVue);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
